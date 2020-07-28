12731830808bd3218ebc754fbd8f3620c3e620ed
Waldman200036 <Waldman200036@gmail.com>
2020-07-27 22:54:22 -0500

    Initial Load

--- lib/cdk-covid-19-stack.ts
+++ lib/cdk-covid-19-stack.ts
@@ -1,9 +1,18 @@
+/* jshint esversion:8 */
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
+// eslint-disable-next-line import/prefer-default-export
 export class CdkCovid19Stack extends cdk.Stack {
+  // eslint-disable-next-line no-useless-constructor
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X, // execution environment
+      code: lambda.Code.fromAsset('lambda'), // code loaded from "lambda" directory
+      handler: 'hello.handler', // file is hello
+    });
     // The code that defines your stack goes here
   }
 }
